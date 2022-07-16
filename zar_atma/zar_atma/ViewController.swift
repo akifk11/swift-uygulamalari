19df4f58cec59eab761bd80e57e8de2c2faa92cb
akifkiraz <akifkrz@gmail.com>
2022-07-16 13:21:08 +0300

    Tasarım

---
+++ zar_atma/zar_atma/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  zar_atma
+//
+//  Created by Mehmet Akif Kiraz on 16.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
